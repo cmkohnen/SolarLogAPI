81dc054ab8e5c1d06c96901c9ce37f420622f2d4
Meloni <christoph.kohnen1@gmail.com>
2020-10-30 17:42:35 +0100

    Cleanup, DatePicker upgrade

--- src/main/java/Interface/DatePicker.java
+++ src/main/java/Interface/DatePicker.java
@@ -2,35 +2,33 @@ package Interface;
 
 import TransformUtilities.DataConversion.Entries;
 import com.github.lgooddatepicker.components.DatePickerSettings;
-import com.github.lgooddatepicker.optionalusertools.DateVetoPolicy;
 
 import java.text.ParseException;
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 public class DatePicker extends com.github.lgooddatepicker.components.DatePicker {
-    public DatePicker(Map<Date, List<Integer>> data) {
+    public DatePicker() {
+    }
+
+    public void addVetoPolicy(Map<Date, List<Integer>> data) {
         DatePickerSettings settings = this.getSettings();
-        settings.setVetoPolicy(new DateVetoPolicy() {
-            @Override
-            public boolean isDateAllowed(LocalDate date) {
-                Date dateasdate = Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
-                boolean bool = false;
-                try {
-                    for(Date timestamp : Entries.entriesperday(dateasdate)) {
-                        if (data.containsKey(dateasdate)) {
-                            bool = true;
-                            break;
-                        }
+        settings.setVetoPolicy(date -> {
+            Date dateasdate = Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            boolean bool = false;
+            try {
+                for(Date timestamp : Entries.entriesperday(dateasdate)) {
+                    if (data.containsKey(dateasdate)) {
+                        bool = true;
+                        break;
                     }
-                } catch (ParseException e) {
-                    e.printStackTrace();
                 }
-                return bool;
+            } catch (ParseException e) {
+                e.printStackTrace();
             }
+            return bool;
         });
     }
 }
