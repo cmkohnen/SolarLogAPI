9bf0123d17d9e43ad481c9d6cb764e3d31022fc2
ChaosMelone9 <christoph.kohnen1@gmail.com>
2021-03-10 11:28:01 +0100

    Add support for Temperature & different inverter functions

--- src/main/java/me/meloni/SolarLogAPI/DataConversion/GetFromJS.java
+++ src/main/java/me/meloni/SolarLogAPI/DataConversion/GetFromJS.java
@@ -2,6 +2,7 @@ package me.meloni.SolarLogAPI.DataConversion;
 
 import me.meloni.SolarLogAPI.FileInteraction.GetFile;
 import me.meloni.SolarLogAPI.FileInteraction.ReadFiles.GetFileContent;
+import me.meloni.SolarLogAPI.Handling.Logger;
 import me.meloni.SolarLogAPI.Inverter;
 import me.meloni.SolarLogAPI.InverterMap;
 import org.apache.commons.lang3.StringUtils;
@@ -42,19 +43,46 @@ public class GetFromJS {
                 // get the timestamp as Date
                 Date date = new SimpleDateFormat(DATEFORMAT).parse(separated.get(0));
                 Map<String, Integer> values = new HashMap<>();
+                int inverterStrings = inverter.strings;
+                if (inverterStrings == 0) inverterStrings = 1;
                 // Get values as String for current inverter
                 List<String> strings = Arrays.asList(separated.get(i + 1).split(";"));
-                //global pac, Index 0
-                values.put("PAC", Integer.valueOf(strings.get(0)));
-                // PDCs, Index string + 1
-                for(int j = 0; j < inverter.strings; j++) {
-                    values.put(String.format("PDC%s", (j + 1)), Integer.valueOf(strings.get(j + 1)));
-                }
-                // Yield for the day, Index amount of strings + 1
-                values.put("YieldDay", Integer.parseInt(strings.get(inverter.strings + 1)));
-                // PDCs, Index amount of strings + 2 + string
-                for(int j = 0; j < inverter.strings; j++) {
-                    values.put(String.format("PDC%s", (j + 1)), Integer.valueOf(strings.get(inverter.strings) + 2 + j));
+                if (inverter.function == 0) {
+                    //global pac, Index 0
+                    values.put("PAC", Integer.valueOf(strings.get(0)));
+                    // PDCs, Index string + 1
+                    for(int j = 0; j < inverterStrings; j++) {
+                        values.put(String.format("PDC%s", (j + 1)), Integer.valueOf(strings.get(j + 1)));
+                    }
+                    // Yield for the day, Index amount of strings + 1
+                    values.put("YieldDay", Integer.parseInt(strings.get(inverterStrings + 1)));
+                    // UDCs, Index amount of strings + 2 + string
+                    for(int j = 0; j < inverterStrings; j++) {
+                        values.put(String.format("UDC%s", (j + 1)), Integer.valueOf(strings.get(inverterStrings) + 2 + j));
+                    }
+                    if (inverter.temperature) {
+                        // Temperature, Index amount of strings * 2 + 2
+                        values.put("Temperature", Integer.valueOf(strings.get(inverterStrings*2 + 2)));
+                    }
+                } else if(inverter.function == 2) {
+                    //global pac, Index 0
+                    values.put("ConsPAC", Integer.valueOf(strings.get(0)));
+                    // PDCs, Index string + 1
+                    for(int j = 0; j < inverterStrings; j++) {
+                        values.put(String.format("ConsPDC%s", (j + 1)), Integer.valueOf(strings.get(j + 1)));
+                    }
+                    // Yield for the day, Index amount of strings + 1
+                    values.put("ConsYieldDay", Integer.parseInt(strings.get(inverterStrings + 1)));
+                    // UDCs, Index amount of strings + 2 + string
+                    for(int j = 0; j < inverterStrings; j++) {
+                        values.put(String.format("ConsUDC%s", (j + 1)), Integer.valueOf(strings.get(inverterStrings) + 2 + j));
+                    }
+                    if (inverter.temperature) {
+                        // Temperature, Index amount of strings * 2 + 2
+                        values.put("Temperature", Integer.valueOf(strings.get(inverterStrings*2 + 2)));
+                    }
+                } else {
+                    Logger.warn("Unsupported inverter function!");
                 }
                 inverterMap.setOnDate(date, values);
             }
@@ -69,7 +97,7 @@ public class GetFromJS {
             if (line.startsWith("WRInfo") && !(StringUtils.substringBetween(line, "(" , ")") == null)) {
                 try {
                     List<String> values = Arrays.asList(StringUtils.substringBetween(line, "(", ")").split(","));
-                    inverters.add(new Inverter(Integer.parseInt(values.get(5)),Integer.parseInt(values.get(11)), values.get(0)));
+                    inverters.add(new Inverter(values.get(0), values.get(4), Integer.parseInt(values.get(5)), Integer.parseInt(values.get(11)), values.get(12).equals("1")));
                 } catch (ArrayIndexOutOfBoundsException ignored) {
                     //Code will get here by multiple strings ("WRInfo[1][6]=new Array("String 1","String 2")"), can be ignored
                 }
--- src/main/java/me/meloni/SolarLogAPI/Inverter.java
+++ src/main/java/me/meloni/SolarLogAPI/Inverter.java
@@ -1,13 +1,17 @@
 package me.meloni.SolarLogAPI;
 
 public class Inverter {
-    public Integer strings;
+    public String type;
     public String identifier;
-    public Integer type;
+    public int strings;
+    public int function;
+    public boolean temperature;
 
-    public Inverter(Integer strings, Integer type, String identifier) {
-        this.strings = strings;
-        this.identifier = identifier;
+    public Inverter(String type, String identifier, int strings, int function, boolean temperature) {
         this.type = type;
+        this.identifier = identifier;
+        this.strings = strings;
+        this.function = function;
+        this.temperature = temperature;
     }
 }
